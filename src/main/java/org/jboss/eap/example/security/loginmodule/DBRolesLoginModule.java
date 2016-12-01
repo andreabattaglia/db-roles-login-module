1ac0dd04033a6d66e18bd2e77f58309eb76e0f52
Andrea Battagla (Red Hat) <abattagl@redhat.com>
2016-12-01 15:43:59 +0000

    Alpha release

--- src/main/java/org/jboss/eap/example/security/loginmodule/DBRolesLoginModule.java
+++ src/main/java/org/jboss/eap/example/security/loginmodule/DBRolesLoginModule.java
@@ -1,30 +1,31 @@
 package org.jboss.eap.example.security.loginmodule;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.security.Principal;
 import java.security.acl.Group;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.kerberos.KerberosPrincipal;
 import javax.security.auth.login.LoginException;
 import javax.sql.DataSource;
 import javax.transaction.SystemException;
 import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 
-import org.jboss.security.PicketBoxLogger;
 import org.jboss.security.PicketBoxMessages;
-import org.jboss.security.auth.spi.UsernamePasswordLoginModule;
-import org.jboss.security.auth.spi.Util;
+import org.jboss.security.SimpleGroup;
+import org.jboss.security.auth.spi.AbstractServerLoginModule;
 import org.jboss.security.plugins.TransactionManagerLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A JDBC based login module that supports authentication and role mapping. It
@@ -55,7 +56,7 @@ import org.jboss.security.plugins.TransactionManagerLocator;
  * @author Scott.Stark@jboss.org
  * @version $Revision$
  */
-public class DBRolesLoginModule extends UsernamePasswordLoginModule {
+public class DBRolesLoginModule extends AbstractServerLoginModule {
     /**
      * Logger for this class
      */
@@ -84,6 +85,8 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
     /** The TransactionManagaer instance to be used */
     protected TransactionManager tm = null;
 
+    protected Principal identity;
+
     /**
      * Initialize this LoginModule.
      * 
@@ -97,6 +100,7 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
      */
     public void initialize(Subject subject, CallbackHandler callbackHandler,
             Map<String, ?> sharedState, Map<String, ?> options) {
+
         addValidOptions(ALL_VALID_OPTIONS);
         super.initialize(subject, callbackHandler, sharedState, options);
         dsJndiName = (String) options.get(DS_JNDI_NAME);
@@ -108,15 +112,16 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
         tmp = options.get(SUSPEND_RESUME);
         if (tmp != null)
             suspendResume = Boolean.valueOf(tmp.toString()).booleanValue();
-
         // Get the Transaction Manager JNDI Name
         String jname = (String) options.get(TRANSACTION_MANAGER_JNDI_NAME);
         if (jname != null)
             this.txManagerJndiName = jname;
 
         LOGGER.trace(
-                "Module options [dsJndiName: {}, rolesQuery: {}, suspendResume: {}]",
+                "Module options [dsJndiName: {}, rolesQuery: {}, suspendResume: {}],",
                 dsJndiName, rolesQuery, suspendResume);
+
+        LOGGER.trace("Shared State: \n {}", sharedState);
 
         try {
             if (this.suspendResume)
@@ -124,29 +129,84 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
         } catch (NamingException e) {
             throw PicketBoxMessages.MESSAGES.failedToGetTransactionManager(e);
         }
+
     }
 
     /**
-     * Get the expected password for the current username available via the
-     * getUsername() method. This is called from within the login() method after
-     * the CallbackHandler has returned the username and candidate password.
+     * Execute the rolesQuery against the dsJndiName to obtain the roles for the
+     * authenticated user.
      * 
-     * @return the valid password String
+     * @return Group[] containing the sets of roles
      */
-    protected String getUsersPassword() throws LoginException {
-        String username = getUsername();
-        String password = null;
+    protected Group[] getRoleSets() throws LoginException {
+        if (rolesQuery != null) {
+            String username = getUsername();
+            LOGGER.trace("Executing query {} with username {}",rolesQuery, username);
+            Group[] roleSets = getRoleSets(username);
+            return roleSets;
+        }
+        return new Group[0];
+    }
+
+    /**
+     * Obtains the username from the identity
+     * @return
+     */
+    private String getUsername() {
+        return getIdentity().getName();
+    }
+
+    /**
+     * A hook to allow subclasses to convert a password from the database into a
+     * plain text string or whatever form is used for matching against the user
+     * input. It is called from within the getUsersPassword() method.
+     * 
+     * @param rawPassword
+     *            - the password as obtained from the database
+     * @return the argument rawPassword
+     */
+    protected String convertRawPassword(String rawPassword) {
+        return rawPassword;
+    }
+
+    protected TransactionManager getTransactionManager()
+            throws NamingException {
+        TransactionManagerLocator tml = new TransactionManagerLocator();
+        return tml.getTM(this.txManagerJndiName);
+    }
+
+    /**
+     * Execute the rolesQuery against the dsJndiName to obtain the roles for the
+     * authenticated user.
+     * 
+     * @return Group[] containing the sets of roles
+     */
+    Group[] getRoleSets(String username) throws LoginException {
+
+        LOGGER.debug("Fetching Role Set for {}", username);
+
         Connection conn = null;
+        HashMap<String, Group> setsMap = new HashMap<String, Group>();
         PreparedStatement ps = null;
         ResultSet rs = null;
 
+        TransactionManager tm = null;
+
+        if (suspendResume) {
+            TransactionManagerLocator tml = new TransactionManagerLocator();
+            try {
+                tm = tml.getTM(txManagerJndiName);
+            } catch (NamingException e1) {
+                throw new RuntimeException(e1);
+            }
+            if (tm == null)
+                throw PicketBoxMessages.MESSAGES
+                        .invalidNullTransactionManager();
+        }
         Transaction tx = null;
         if (suspendResume) {
             // tx = TransactionDemarcationSupport.suspendAnyTransaction();
             try {
-                if (tm == null)
-                    throw PicketBoxMessages.MESSAGES
-                            .invalidNullTransactionManager();
                 tx = tm.suspend();
             } catch (SystemException e) {
                 throw new RuntimeException(e);
@@ -157,18 +217,41 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
             InitialContext ctx = new InitialContext();
             DataSource ds = (DataSource) ctx.lookup(dsJndiName);
             conn = ds.getConnection();
-            // Get the password
-            PicketBoxLogger.LOGGER.traceExecuteQuery(principalsQuery, username);
-            ps = conn.prepareStatement(principalsQuery);
-            ps.setString(1, username);
+            // Get the user role names
+            LOGGER.trace("Executing query %s with username %s",rolesQuery, username);
+            ps = conn.prepareStatement(rolesQuery);
+            try {
+                ps.setString(1, username);
+            } catch (ArrayIndexOutOfBoundsException ignore) {
+                // The query may not have any parameters so just try it
+            }
             rs = ps.executeQuery();
             if (rs.next() == false) {
                 throw PicketBoxMessages.MESSAGES
-                        .noMatchingUsernameFoundInPrincipals();
+                        .noMatchingUsernameFoundInRoles();
             }
 
-            password = rs.getString(1);
-            password = convertRawPassword(password);
+            do {
+                String name = rs.getString(1);
+                String groupName = rs.getString(2);
+                if (groupName == null || groupName.length() == 0)
+                    groupName = "Roles";
+                Group group = (Group) setsMap.get(groupName);
+                if (group == null) {
+                    group = new SimpleGroup(groupName);
+                    setsMap.put(groupName, group);
+                }
+
+                try {
+                    Principal p = createIdentity(name);
+                    group.addMember(p);
+                } catch (Exception e) {
+                    LOGGER.error("Failed to create principal {}",name,
+                            e);
+                }
+            } while (rs.next());
+
+            LOGGER.debug("Role Set obtained={}", setsMap);
         } catch (NamingException ex) {
             LoginException le = new LoginException(PicketBoxMessages.MESSAGES
                     .failedToLookupDataSourceMessage(dsJndiName));
@@ -195,7 +278,7 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
             if (conn != null) {
                 try {
                     conn.close();
-                } catch (SQLException ex) {
+                } catch (Exception ex) {
                 }
             }
             if (suspendResume) {
@@ -207,42 +290,23 @@ public class DBRolesLoginModule extends UsernamePasswordLoginModule {
                 }
             }
         }
-        return password;
+
+        Group[] roleSets = new Group[setsMap.size()];
+        setsMap.values().toArray(roleSets);
+        return roleSets;
     }
 
-    /**
-     * Execute the rolesQuery against the dsJndiName to obtain the roles for the
-     * authenticated user.
-     * 
-     * @return Group[] containing the sets of roles
-     */
-    protected Group[] getRoleSets() throws LoginException {
-        if (rolesQuery != null) {
-            String username = getUsername();
-            PicketBoxLogger.LOGGER.traceExecuteQuery(rolesQuery, username);
-            Group[] roleSets = Util.getRoleSets(username, dsJndiName,
-                    txManagerJndiName, rolesQuery, this, suspendResume);
-            return roleSets;
+    @Override
+    protected Principal getIdentity() {
+
+        if (identity == null) {
+            identity = subject.getPrincipals(KerberosPrincipal.class).iterator()
+                    .next();
+            LOGGER.debug(
+                    "Identity obtained from previous LoginModule : classname={} name={}",
+                    identity.getClass(), identity.getName());
         }
-        return new Group[0];
-    }
 
-    /**
-     * A hook to allow subclasses to convert a password from the database into a
-     * plain text string or whatever form is used for matching against the user
-     * input. It is called from within the getUsersPassword() method.
-     * 
-     * @param rawPassword
-     *            - the password as obtained from the database
-     * @return the argument rawPassword
-     */
-    protected String convertRawPassword(String rawPassword) {
-        return rawPassword;
-    }
-
-    protected TransactionManager getTransactionManager()
-            throws NamingException {
-        TransactionManagerLocator tml = new TransactionManagerLocator();
-        return tml.getTM(this.txManagerJndiName);
+        return identity;
     }
 }
